cc958b36e0c94cd588f33a878ac6649d58221f87
Mugurel Moscaliuc <mugur.moscaliuc@gmail.com>
2021-05-10 00:57:55 +0100

    Initial Commit

---
+++ PaginationExample/PaginationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaginationExampleApp.swift
+//  PaginationExample
+//
+//  Created by Mugurel Moscaliuc on 10/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct PaginationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
